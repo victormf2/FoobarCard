61f8c9d97ca7bab8221e3e00ce6b17266c60abe3
Victor Machado <victor_mf2@hotmail.com>
2011-10-01 01:52:44 -0300

    Persistencia funcionando

--- src/main/java/br/com/foobar/Testeee.java
+++ src/main/java/br/com/foobar/Testeee.java
@@ -3,20 +3,15 @@ package br.com.foobar;
 import br.com.foobar.persistence.Card;
 import br.com.foobar.persistence.CardDAO;
 import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.sql.SQLException;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 public class Testeee {
-    
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         CardDAO c = new CardDAO();
-        c.insert(new Card("99999999777", new BigDecimal(12.3)));
+        c.insert(new Card("999567578", new BigDecimal(12.3)));
         List<Card> cards = c.getCards();
-        for(Card card : cards) {
+        for (Card card : cards) {
             System.out.println(card);
         }
     }
--- src/main/java/br/com/foobar/persistence/CardDAO.java
+++ src/main/java/br/com/foobar/persistence/CardDAO.java
@@ -1,28 +1,27 @@
 package br.com.foobar.persistence;
 
-import java.sql.SQLException;
 import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class CardDAO {
-    
+
     private SessionFactory factory;
 
     public CardDAO() {
-        factory = new Configuration().addAnnotatedClass(Card.class).buildSessionFactory();
+        factory = new Configuration().configure().buildSessionFactory();
     }
-    
+
     public void insert(Card card) {
         Session session = factory.openSession();
-        session.save(card);  
-        session.flush();
+        session.beginTransaction();
+        session.save(card);
+        session.getTransaction().commit();
         session.close();
     }
-    
+
     public List<Card> getCards() {
         Session session = factory.openSession();
         Criteria c = session.createCriteria(Card.class);
